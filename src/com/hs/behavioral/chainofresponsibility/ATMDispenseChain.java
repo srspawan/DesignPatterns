28dc48bc56807fa23d9b0fbffbd01c7e4feb1931
hareramcse <hareramcse@gmail.com>
2019-03-03 23:52:37 +0530

    added builder design pattern

--- src/com/hs/behavioral/chainofresponsibility/ATMDispenseChain.java
+++ src/com/hs/behavioral/chainofresponsibility/ATMDispenseChain.java
@@ -8,7 +8,7 @@ public class ATMDispenseChain {
 
 	public ATMDispenseChain() {
 		// initialize the chain
-		this.chain1 = new Dollar50Dispenser(); 
+		this.chain1 = new Dollar50Dispenser();
 		DispenseChain chain2 = new Dollar20Dispenser();
 		DispenseChain chain3 = new Dollar10Dispenser();
 
@@ -22,11 +22,12 @@ public class ATMDispenseChain {
 		while (true) {
 			int amount = 0;
 			System.out.println("Enter amount to dispense");
-			Scanner input = new Scanner(System.in);
-			amount = input.nextInt();
-			if (amount % 10 != 0) {
-				System.out.println("Amount should be in multiple of 10s.");
-				return;
+			try (Scanner input = new Scanner(System.in)) {
+				amount = input.nextInt();
+				if (amount % 10 != 0) {
+					System.out.println("Amount should be in multiple of 10s.");
+					return;
+				}
 			}
 			// process the request
 			atmDispenser.chain1.dispense(new Currency(amount));
